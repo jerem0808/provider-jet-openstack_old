6d98d7c3abfffff091bff85e08401772ea89d606
Weber Jeremy <jeremy.weber@swiscom.com>
2022-06-14 15:26:56 +0200

    Last changes

--- config/provider.go
+++ config/provider.go
@@ -23,12 +23,12 @@ import (
 	tjconfig "github.com/crossplane/terrajet/pkg/config"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
-	"github.com/crossplane-contrib/provider-jet-template/config/null"
+	"github.com/crossplane-contrib/provider-jet-openstack/config/null"
 )
 
 const (
-	resourcePrefix = "template"
-	modulePath     = "github.com/crossplane-contrib/provider-jet-template"
+	resourcePrefix = "openstack"
+	modulePath     = "github.com/crossplane-contrib/provider-jet-openstack"
 )
 
 //go:embed schema.json
